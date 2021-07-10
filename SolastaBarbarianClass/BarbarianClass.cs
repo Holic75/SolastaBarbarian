fb6d77c595b060052938097ab6c0c80fcbbd3c77
Holic75 <holic.denis@gmail.com>
2021-07-11 01:41:00 +0200

    Update BarbarianClass.cs
    - fixed war shaman spell progression to be inline with other spell casters

--- SolastaBarbarianClass/BarbarianClass.cs
+++ SolastaBarbarianClass/BarbarianClass.cs
@@ -824,7 +824,7 @@ namespace SolastaBarbarianClass
                                                                                               "Feature/&BarbarianSubclassWarShamanClassSpellcastingDescription",
                                                                                               war_shaman_spelllist,
                                                                                               Helpers.Stats.Wisdom,
-                                                                                              new List<int> {0, 0, 2, 2, 2, 2, 2, 2, 2, 2,
+                                                                                              new List<int> {0, 0, 2, 2, 2, 2, 2, 2, 2, 3,
                                                                                                              3, 3, 3, 3, 3, 3, 3, 3, 3, 3 },
                                                                                               new List<int> { 0,  0,  3,  4,  4,  4,  5,  6,  6,  7,
                                                                                                               8,  8,  9, 10, 10, 11, 11, 11, 12, 13},
